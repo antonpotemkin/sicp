2a9bb7dcf383d85477e0c3ae850a067541137a79
Anton Potemkin <79133891911@yandex.ru>
2019-01-20 13:04:09 +0700

    fix task 2_07

--- test/chapter02/test_07.rkt
+++ test/chapter02/test_07.rkt
@@ -3,6 +3,19 @@
 (require rackunit "../../src/chapter02/solution_07.rkt")
 
 (define interval (make-interval 20 30))
+(define interval2 (make-interval 10 50))
+(define add (add-interval interval interval2))
+(define mul (mul-interval interval interval2))
+(define div (div-interval interval interval2))
 
-(check-equal? (upper-bound interval) 20)
-(check-equal? (lower-bound interval) 30)
+(check-equal? (upper-bound interval) 30)
+(check-equal? (lower-bound interval) 20)
+
+(check-equal? (upper-bound add) 80)
+(check-equal? (lower-bound add) 30)
+
+(check-equal? (upper-bound mul) 1500)
+(check-equal? (lower-bound mul) 200)
+
+(check-equal? (upper-bound div) 3.0)
+(check-equal? (lower-bound div) 0.4)
