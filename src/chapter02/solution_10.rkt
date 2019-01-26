0f172075dba9bb9e2515f3d3593d3fc828eed6c3
Anton Potemkin <79133891911@yandex.ru>
2019-01-27 01:40:29 +0700

    fix typo

--- src/chapter02/solution_10.rkt
+++ src/chapter02/solution_10.rkt
@@ -2,8 +2,6 @@
 
 (require rackunit "./solution_07.rkt")
 
-(define (make-interval a b) (cons a b))
-
 (define (div2-interval x y)
   (if (and (<= (lower-bound y) 0)
            (>= (upper-bound y) 0))
